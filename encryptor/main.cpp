3a7a57a606cb1ca402873c31bc3937f959dd98eb
svc64 <>
2020-09-27 19:14:51 +0300

    Initial Commit

---
+++ encryptor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  encryptor
+//
+//  Created by svc64 on 9/27/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
